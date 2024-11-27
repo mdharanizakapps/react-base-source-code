6fe1ae22b1914ce57497084c6e520c506fa240cc
dganesan <dganesan@smythstoys.com>
2024-11-27 13:17:47 +0530

    Updated Alert Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import React, { useState } from 'react'; // Import React
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React from 'react'; // Import React
 import './App.css';
 import './index.css'; // Import your Tailwind CSS file
+import { AlertComponent } from './components/ui_library_components/AlertComponent';
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Alert
+        </div>
+        <div>
+          <AlertComponent></AlertComponent>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }

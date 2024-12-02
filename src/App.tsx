fb2cce0c5e867f29e81c6cb71a4288e00e38c79d
dganesan <dganesan@smythstoys.com>
2024-12-02 15:10:20 +0530

    Updated App.tsx with all the component title

--- src/App.tsx
+++ src/App.tsx
@@ -90,6 +90,366 @@ function App() {
         </div>
       </div>
 
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Card
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Carousel
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Chart
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Checkbox
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Collapsible
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Combobox
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Command
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Context Menu
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Data Table
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Date Picker
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Dialog
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Drawer
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Dropdown Menu
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Form
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Hover Card
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Input
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Input OTP
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Label
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Menubar
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Navigation Menu
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Pagination
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Popover
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Progress
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Radio Group
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Resizable
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Scroll Area
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Select
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Separator
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Sheet
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Skeleton
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Slider
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Sonner
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Switch
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Table
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Tabs
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Textarea
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Toast
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Toggle
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Toggle Group
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <div>
+        <div className='text-center text-lg font-bold text-blue-600'>
+          Tooltip
+        </div>
+        <div>
+
+        </div>
+      </div>
+
 
 
 
